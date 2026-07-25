cb25edf23fa7ea2eca8715b41f8c6979e148761d
gar <106908475+garlikethefish@users.noreply.github.com>
2026-07-25 12:57:36 +0300

    stuff

--- objects/interactables/button.gd
+++ objects/interactables/button.gd
@@ -3,11 +3,11 @@ extends Area2D
 @onready var sprite := %Sprite2D
 var activated := false
 
-func _on_body_entered(body: Node2D) -> void:
-	# if player steps on button, then button is activated
-	if body.is_in_group("player"):
-		sprite.region_rect(32,0,32,32)
-		activated = true
+#func _on_body_entered(body: Node2D) -> void:
+	## if player steps on button, then button is activated
+	#if body.is_in_group("player"):
+		#sprite.region_rect(32,0,32,32)
+		#activated = true
 
 func activate():
 	pass
